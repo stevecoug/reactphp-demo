b81b60143d9aa2ca547dcb66c711c4eda1a681b3
Steve Meyers <steve-github@spamwiz.com>
2014-06-17 16:27:50 -0600

    added http server to chat server, to show multiple servers in one loop

--- bin/chat.php
+++ bin/chat.php
@@ -3,7 +3,18 @@
 
 require_once __DIR__."/../vendor/autoload.php";
 
-$app = new Ratchet\App("chat.local", 8080);
+$loop = React\EventLoop\Factory::create();
+
+$http_socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($http_socket);
+$http->on('request', function ($request, $response) {
+        $response->writeHead(200, [ 'Content-Type' => 'text/plain' ]);
+        $response->end("Hello World!\n");
+});
+
+$http_socket->listen(8000);
+
+$app = new Ratchet\App("chat.local", 8080, "127.0.0.1", $loop);
 $app->route("/chat", new Demo\Chat());
 $app->run();
 
